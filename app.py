48975655f143f92bd10053db1437eab6d136f3dc
tiffanyng23 <ngtiffany23@gmail.com>
2026-03-19 20:55:22 -0400

    creating contact form

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
+import smtplib
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
 # Home Page 
 @app.route("/")
-def main():
-    #insert slideshow of images
+def homepage():
     return render_template("homepage.html")
 
 # About Me
@@ -15,16 +15,23 @@ def main():
 def about():
     return render_template("about.html")
 
-# Contact
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
 # Projects
-# Earthquake Tracker Dashboard
 @app.route("/projects")
-def earthquakes():
+def projects():
     return render_template("projects.html")
+
+# Contact
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        # user submits form, email gets sent, landing page confirms sent email
+        user_email = request.form.get("user_email")
+        subject = request.form.get("subject")
+        message = request.form.get("message")
+        return render_template("landing.html")
+
+    else:
+        return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
